6e03ac28962466ec9037ee1c921912662059b0d5
byteford <sandford.james@gmail.com>
2022-04-04 14:33:44 +0100

    feat: adding react strict

--- front-end/src/src/index.js
+++ front-end/src/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
 
 
 reportWebVitals();
