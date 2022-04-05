36b587a4142d805c4178db590191d98b264503ee
byteford <sandford.james@gmail.com>
2022-04-05 12:01:30 +0100

    feat: removing unneeded code

--- front-end/src/lowerthird.js
+++ front-end/src/lowerthird.js
@@ -29,21 +29,7 @@ export default class Lowerthird extends React.Component{
     componentWillUnmount(){
         clearInterval(this.interval)
     }
-    shouldComponentUpdate(nextProps,nextState){
-        if (nextState.name !== this.state.name){
-            return true
-        }
-        if (nextState.role !== this.state.role){
-            return true
-        }
-        if (nextState.social !== this.state.social){
-            return true
-        }
-        if(nextProps){
-            return true
-        }
-        return false
-    }
+    
     render(){
         const url = "https://ujr0uayvh8.execute-api.eu-west-2.amazonaws.com/overlay/lowerthird?role="+this.state.role+"&social="+this.state.social+"&name="+this.state.name+"&role_size="+this.props.config.Role.Font_size+"&role_loc_x="+this.props.config.Role.X+"&role_loc_y="+this.props.config.Role.Y+"&social_size"+this.props.config.Social.Font_size+"&social_loc_x="+this.props.config.Social.X+"&social_loc_y="+this.props.config.Social.Y+"&name_size="+this.props.config.Name.Font_size+"&name_loc_x="+this.props.config.Name.X+"&name_loc_y="+this.props.config.Name.Y
         return (
