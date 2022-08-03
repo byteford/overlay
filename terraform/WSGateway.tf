ce5ff5c7c2fd69e3f2362261e187ac31a8084530
byteford <sandford.james@gmail.com>
2022-08-03 14:15:02 +0100

    feat: building out websocket

---
+++ terraform/WSGateway.tf
@@ -0,0 +1,28 @@
+resource "aws_apigatewayv2_api" "ws" {
+  name          = "wsoverlay"
+  protocol_type = "WEBSOCKET"
+  route_selection_expression = "$request.body.action"
+
+}
+
+resource "aws_apigatewayv2_stage" "ws" {
+  api_id      = aws_apigatewayv2_api.ws.id
+  name        = "overlay"
+  auto_deploy = true
+  default_route_settings {
+    detailed_metrics_enabled = true
+    throttling_burst_limit   = 100
+    throttling_rate_limit    = 100
+  }
+}
+
+resource "aws_apigatewayv2_route" "connect" {
+  api_id    = aws_apigatewayv2_api.ws.id
+  route_key = "$connect"
+  target = "intergrations/${aws_apigatewayv2_integration.connect.id}"
+}
+
+resource "aws_apigatewayv2_integration" "connect" {
+  api_id           = aws_apigatewayv2_api.ws.id
+  integration_type = "MOCK"
+}
