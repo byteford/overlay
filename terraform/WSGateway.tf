bb19297bc92f2ed08af852f735d7330c7156c083
byteford <sandford.james@gmail.com>
2022-08-03 14:35:21 +0100

    fix: setting the intergration

--- terraform/WSGateway.tf
+++ terraform/WSGateway.tf
@@ -24,8 +24,6 @@ resource "aws_apigatewayv2_route" "connect" {
 
 resource "aws_apigatewayv2_integration" "connect" {
   api_id                 = aws_apigatewayv2_api.ws.id
-  integration_type       = "AWS_PROXY"
-  connection_type        = "INTERNET"
-  payload_format_version = "2.0"
+  integration_type       = "AWS"
   integration_uri        = aws_lambda_function.ws_connect.invoke_arn
 }
