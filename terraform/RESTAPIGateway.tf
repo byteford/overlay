f59d1753246f3efbaf1d7d74eb0a4abc9703e534
byteford <sandford.james@gmail.com>
2022-08-10 10:58:42 +0100

    fix: quotes mising

--- terraform/RESTAPIGateway.tf
+++ terraform/RESTAPIGateway.tf
@@ -57,7 +57,7 @@ resource "aws_api_gateway_rest_api" "overlay" {
     }
     definitions :{
       valuejson :{
-        type: string
+        type: "string"
         properties:{
           Value:{
             type: "string"
